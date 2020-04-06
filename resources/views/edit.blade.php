6070f8d99ee92472de541c222483592c31eaf1d9
bonysinurat <60059439+bonysinurat@users.noreply.github.com>
2020-04-06 09:57:15 +0700

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,91 +1,72 @@
 @extends('layouts.app')
-
 @include('header')
 
+
 <!--edit.blade-->
+<div class="container">
 <div class="row" style="margin-left: 15%;">
 
 <div class="col-sm-9" >
-<h3 style="margin-bottom:40px; font-weight:bold;">Akun Profil</h3>
+<h3 style="margin-bottom:10px; font-weight:bold;text-align: center; color:#51ABA4">AKUN</h3>
+<a class="edit" href="editprofil" style="font-size:1.5em"><img src=" {{ ('ikon/pencil.png') }}" style="width:23px; height:23px">&nbsp; Edit Akun</a>
 
-<form>
-<div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Nama Lengkap</label>
+<form name="form" style="margin-bottom:40%">
+<div class="form-group row" style="margin-top:25px">
+    <label for="inputNama" class="col-sm-3 col-form-label" >Nama Lengkap</label>
     <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword"  >
+      <input type="text" class="form-control" id="inputPassword" name="nama" disabled>
     </div>
   </div>
 
   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
+      <label for="InputEmail" class="col-sm-3 col-form-label" >Email</label>
     <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword" >
+      <input type="email" class="form-control" id="InputEmail" name="email" disabled>
     </div>
   </div>
 
   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Email</label>
+    <label for="inputNomorHp" class="col-sm-3 col-form-label" >No. Telepon</label>
     <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword" >
+      <input type="text" class="form-control" id="inputPassword" name="noHp" disabled>
     </div>
   </div>
 
   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">No. Telp</label>
+    <label for="inputPekerjaan" class="col-sm-3 col-form-label" >Pekerjaan</label>
     <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword" >
+      <input type="text" class="form-control" id="inputPassword" name="pekerjaan" disabled>
     </div>
   </div>
 
   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Status</label>
+    <label for="inputNoId" class="col-sm-3 col-form-label" >Nomor Identitas</label>
     <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword">
+      <input type="text" class="form-control" id="inputPassword" name="noId" disabled>
     </div>
   </div>
 
   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">No. ID</label>
+    <label for="inputAlamat" class="col-sm-3 col-form-label" >Alamat</label>
     <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword" >
+      <input type="text" class="form-control" id="inputPassword" name="alamat" disabled>
     </div>
   </div>
 
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Alamat</label>
-    <div class="col-sm-7">
-      <input type="text" class="form-control" id="inputPassword" >
-    </div>
-  </div>
-
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Kata Sandi</label>
-    <div class="col-sm-7">
-      <input type="password" class="form-control" id="inputPassword" >
-    </div>
-  </div>
-
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Konfirmasi Kata Sandi</label>
-    <div class="col-sm-7">
-      <input type="password" class="form-control" id="inputPassword" >
-    </div>
-  </div>
-</form>
-
-<div style="float:right; margin-right:25%;">
-<button type="button" class="btn btn-danger" style="margin-right:20px;">Batal</button>
-<button type="button" class="btn btn-success">Ok</button>
+  </form>
 </div>
 
-</div>
+<div class="col-sm-3" style="margin-top:70px;">
 
-<div class="col-sm-3" style="margin-left: 0;">
 <img src ="{{ ('ikon/foto.jpg') }}" style="width:200px">
-<button type="button" class="btn btn-dark" style="margin-left:60px; margin-top:10px;">pilih File</button>
-
 </div>
 
 </div>
+
+</div>
+
+
+  
 
 @include('footer')
+
